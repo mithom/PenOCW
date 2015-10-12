32faece26c233c9461da5ef75a688717ce0d4f40
NeleN <nele.nauwelaers@hotmail.be>
2015-10-12 16:35:14 +0200

    rijden

--- Team_auto/Car.py
+++ Team_auto/Car.py
@@ -7,29 +7,18 @@ BrickPi.MotorEnable[PORT_B] = 1 #Enable the Motor B
 
 BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
 
-class Car:
-    def __init__(self, power):
-        self.power = power
-        
-    def set_left(power):
-        BrickPi.MotorSpeed[PORT_A] = power  #Set the speed of MotorA (-255 to 255)
+def set_left(power):
+    BrickPi.MotorSpeed[PORT_A] = power  #Set the speed of MotorA (-255 to 255)
 
-    def set_right(power):
-        BrickPi.MotorSpeed[PORT_B] = power  #Set the speed of MotorB (-255 to 255)
+def set_right(power):
+    BrickPi.MotorSpeed[PORT_B] = power  #Set the speed of MotorB (-255 to 255)
 
-    def stop(self):
-        BrickPi.MotorSpeed[PORT_A] = 0
-        BrickPi.MotorSpeed[PORT_B] = 0
+def turn_straight_left(power):
+    BrickPi.MotorSpeed[PORT_A] = - power
+    BrickPi.MotorSpeed[PORT_B] = power
 
-    def turn_straight_left(self, power):
-        BrickPi.MotorSpeed[PORT_A] = - power
-        BrickPi.MotorSpeed[PORT_B] = power
+def turn_straight_right(power):
+    BrickPi.MotorSpeed[PORT_A] = power
+    BrickPi.MotorSpeed[PORT_B] = - power
 
-    def turn_straight_right(self, power):
-        BrickPi.MotorSpeed[PORT_A] = power
-        BrickPi.MotorSpeed[PORT_B] = - power
-
-
-auto = Car(100)
-auto.set_left(100)
-auto.set_right(-100)
+set_left(100)

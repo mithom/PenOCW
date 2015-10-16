63649a6d514499008029045bcd2b1614ebff9463
Jasper Mariën <jasper.marien1@student.kuleuven.be>
2015-10-16 09:13:03 +0000

    go_straight()

--- Team_auto/Car.py
+++ Team_auto/Car.py
@@ -1,4 +1,5 @@
 from BrickPi import *   #import BrickPi.py file to use BrickPi operations
+import time
 
 BrickPiSetup()  # setup the serial port for communicationfrom BrickPi import *
 
@@ -14,21 +15,41 @@ def set_right(power):
     BrickPi.MotorSpeed[PORT_B] = power  #Set the speed of MotorB (-255 to 255)
 
 def turn_straight_left(power):
-    BrickPi.MotorSpeed[PORT_A] = - power
-    BrickPi.MotorSpeed[PORT_B] = power
+    set_left(-power)
+    set_right(power)
     BrickPiUpdateValues()
 
 def turn_straight_right(power):
-    BrickPi.MotorSpeed[PORT_A] = power
-    BrickPi.MotorSpeed[PORT_B] = - power
-    BrickPiUpdateValues()
-
-def go_straight(power):
     set_left(power)
-    set_right(power)
-
+    set_right(-power)
     BrickPiUpdateValues()
 
+def go_straight(power, duration):
+	BrickPi.nMotorEncoder[PORT_A] = 0 #reset the value of encoder A to zero
+	BrickPi.nMotorEncoder[PORT_B] = 0 #reset the value of encoder B to zero
+	left_power = power
+	right_power = power
+	start_time = time.clock()
+	while (time.clock() - start_time < duration):
+		if (BrickPi.nMotorEncoder[PORT_A] +10 > BrickPi.nMotorEncoder[PORT_B]) and (BrickPi.nMotorEncoder[PORT_B] +10 > BrickPi.nMotorEncoder[PORT_A]):
+			set_left(left_power)
+			set_right(right_power)
+			BrickPiUpdateValues()
+		elif (BrickPi.nMotorEncoder[PORT_A] > BrickPi.nMotorEncoder[PORT_B]):
+			correction_factor = "blehbleh"
+			left_power -= correction_factor
+			right_power += correction_factor
+			set_left(left_power)
+			set_right(right_power)
+			BrickPiUpdateValues()
+		else:
+			correction_factor = "blehbleh"
+			left_power += correction_factor
+			right_power -= correction_factor
+			set_left(left_power)
+			set_right(right_power)
+			BrickPiUpdateValues
+			
 def make_circle_left(power, radius)
     left_power = int (((radius - 11.5)/ radius)*power)
     set_left(left_power)

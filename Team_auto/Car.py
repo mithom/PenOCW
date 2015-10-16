c9ec9c9c949a3c4c516187c814373ded228b2b70
Jasper Mariën <jasper.marien1@student.kuleuven.be>
2015-10-16 11:06:26 +0000

    rotate_angle_left/right & alfabatisch

--- Team_auto/Car.py
+++ Team_auto/Car.py
@@ -5,24 +5,10 @@ BrickPiSetup()  # setup the serial port for communicationfrom BrickPi import *
 
 BrickPi.MotorEnable[PORT_A] = 1 #Enable the Motor A
 BrickPi.MotorEnable[PORT_B] = 1 #Enable the Motor B
+car_width = 11.5
+wheel_contour = 5 #foute waarde
 
 BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
-       
-def set_left(power):
-    BrickPi.MotorSpeed[PORT_A] = power  #Set the speed of MotorA (-255 to 255)
-
-def set_right(power):
-    BrickPi.MotorSpeed[PORT_B] = power  #Set the speed of MotorB (-255 to 255)
-
-def turn_straight_left(power):
-    set_left(-power)
-    set_right(power)
-    BrickPiUpdateValues()
-
-def turn_straight_right(power):
-    set_left(power)
-    set_right(-power)
-    BrickPiUpdateValues()
 
 def go_straight(power, duration):
 	BrickPi.nMotorEncoder[PORT_A] = 0 #reset the value of encoder A to zero
@@ -50,9 +36,45 @@ def go_straight(power, duration):
 			set_right(right_power)
 			BrickPiUpdateValues
 			
-def make_circle_left(power, radius)
-    left_power = int (((radius - 11.5)/ radius)*power)
+def make_circle_left(power, radius):
+    left_power = int (((radius - car_width)/ radius)*power)
     set_left(left_power)
     set_right (power)
+    BrickPiUpdateValues()
+	
+def make_circle_right(power, radius):
+    right_power = int (((radius - car_width)/ radius)*power)
+    set_left(power)
+    set_right (right_power)
+    BrickPiUpdateValues()
+	
+def rotate_angle_left(power, angle):
+	"""Angle in radians"""
+	BrickPi.nMotorEncoder[PORT_B] = 0
+	goal_angle_wheel = int((angle*car_width*360)/(2*wheel_contour)) #in graden
+	while (BrickPi.nMotorEncoder[PORT_B] < goal_angle_wheel):
+		turn_straight_left(power)
+		
+def rotate_angle_right(power, angle):
+	"""Angle in radians"""
+	BrickPi.nMotorEncoder[PORT_A] = 0
+	goal_angle_wheel = int((angle*car_width*360)/(2*wheel_contour)) #in graden
+	while (BrickPi.nMotorEncoder[PORT_A] < goal_angle_wheel):
+		turn_straight_right(power)
+       
+def set_left(power):
+    BrickPi.MotorSpeed[PORT_A] = power  #Set the speed of MotorA (-255 to 255)
+
+def set_right(power):
+    BrickPi.MotorSpeed[PORT_B] = power  #Set the speed of MotorB (-255 to 255)
+
+def turn_straight_left(power):
+    set_left(-power)
+    set_right(power)
+    BrickPiUpdateValues()
+
+def turn_straight_right(power):
+    set_left(power)
+    set_right(-power)
     BrickPiUpdateValues()
 

07f3b182101fa3561899954cb1e7cfd824262c0a
Thomas Michiels <mi_thom@hotmail.com>
2015-10-19 15:16:24 +0200

    Revert "?????"
    This reverts commit 4933dc7531ae7dfab57054e85fecd037994bbde4.

--- Team_auto/Car.py
+++ Team_auto/Car.py
@@ -1,37 +1,121 @@
-from BrickPi import *   #import BrickPi.py file to use BrickPi operations
+from BrickPi import *  # import BrickPi.py file to use BrickPi operations
+import time
 
 BrickPiSetup()  # setup the serial port for communicationfrom BrickPi import *
 
-BrickPi.MotorEnable[PORT_A] = 1 #Enable the Motor A
-BrickPi.MotorEnable[PORT_B] = 1 #Enable the Motor B
+BrickPi.MotorEnable[PORT_A] = 1  # Enable the Motor A
+BrickPi.MotorEnable[PORT_B] = 1  # Enable the Motor B
+car_width = 11.5
+wheel_contour = 5  # foute waarde
 
-BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
-       
+BrickPiSetupSensors()  # Send the properties of sensors to BrickPi
+
+
+def execute_function_with_id(function_id, *args):
+    for f in functions:
+        if id(f) == function_id:
+            f(*args)
+
+
+def get_function_ids():
+    function_ids = {'go_straight': id(go_straight), 'make_circle_left': id(make_circle_left),
+                    'make_circle_right': id(make_circle_right), 'rotate_angle_left': id(rotate_angle_left),
+                    'rotate_angle_right': id(rotate_angle_left),
+                    'set_left': id(set_left), 'set_right': id(set_right), 'turn_straight_left': id(turn_straight_left),
+                    'turn_straight_right': id(turn_straight_right)}
+    return function_ids
+
+
+def go_straight(power, duration):
+    BrickPi.nMotorEncoder[PORT_A] = 0  # reset the value of encoder A to zero
+    BrickPi.nMotorEncoder[PORT_B] = 0  # reset the value of encoder B to zero
+    left_power = power
+    right_power = power
+    start_time = time.clock()
+    while (time.clock() - start_time) < duration:
+        if (BrickPi.nMotorEncoder[PORT_A] + 10 > BrickPi.nMotorEncoder[PORT_B]) and (
+                BrickPi.nMotorEncoder[PORT_B] + 10 > BrickPi.nMotorEncoder[PORT_A]):
+            set_left(left_power)
+            set_right(right_power)
+            BrickPiUpdateValues()
+        elif BrickPi.nMotorEncoder[PORT_A] > BrickPi.nMotorEncoder[PORT_B]:
+            correction_factor = int(0.05*power)
+            left_power -= correction_factor
+            set_left(left_power)
+            set_right(right_power)
+            BrickPiUpdateValues()
+        else:
+            correction_factor = int(0.05*power)
+            right_power -= correction_factor
+            set_left(left_power)
+            set_right(right_power)
+            BrickPiUpdateValues()
+
+
+def make_circle_left(power, radius):
+    left_power = int(((radius - car_width) / radius) * power)
+    set_left(left_power)
+    set_right(power)
+    BrickPiUpdateValues()
+
+
+def make_circle_right(power, radius):
+    right_power = int(((radius - car_width) / radius) * power)
+    set_left(power)
+    set_right(right_power)
+    BrickPiUpdateValues()
+
+
+def rotate_angle_left(power, angle):
+    """Angle in radians"""
+    BrickPi.nMotorEncoder[PORT_B] = 0
+    goal_angle_wheel = int((angle * car_width * 360) / (2 * wheel_contour))  # in graden
+    while BrickPi.nMotorEncoder[PORT_B] < goal_angle_wheel:
+        turn_straight_left(power)
+
+
+def rotate_angle_right(power, angle):
+    """Angle in radians"""
+    BrickPi.nMotorEncoder[PORT_A] = 0
+    goal_angle_wheel = int((angle * car_width * 360) / (2 * wheel_contour))  # in graden
+    while BrickPi.nMotorEncoder[PORT_A] < goal_angle_wheel:
+        turn_straight_right(power)
+
+
 def set_left(power):
-    BrickPi.MotorSpeed[PORT_A] = power  #Set the speed of MotorA (-255 to 255)
+    BrickPi.MotorSpeed[PORT_A] = power  # Set the speed of MotorA (-255 to 255)
+
 
 def set_right(power):
-    BrickPi.MotorSpeed[PORT_B] = power  #Set the speed of MotorB (-255 to 255)
+    BrickPi.MotorSpeed[PORT_B] = power  # Set the speed of MotorB (-255 to 255)
+
 
 def turn_straight_left(power):
-    BrickPi.MotorSpeed[PORT_A] = - power
-    BrickPi.MotorSpeed[PORT_B] = power
+    set_left(-power)
+    set_right(power)
     BrickPiUpdateValues()
+
 
 def turn_straight_right(power):
-    BrickPi.MotorSpeed[PORT_A] = power
-    BrickPi.MotorSpeed[PORT_B] = - power
-    BrickPiUpdateValues()
-
-def go_straight(power):
     set_left(power)
-    set_right(power)
-
+    set_right(-power)
     BrickPiUpdateValues()
 
-def make_circle_left(power, radius)
-    left_power = int (((radius - 11.5)/ radius)*power)
-    set_left(left_power)
-    set_right (power)
-    BrickPiUpdateValues()
 
+def stop():
+    set_right(0)
+    set_left(0)
+    BrickUpdateValues()
+
+
+def brake():
+    while(power>5):
+        set_left(power-10)
+        set_right(power-10)
+        power -= 10
+    set_left(0)
+    set_right(0)
+    power = 0
+
+functions = [go_straight, make_circle_left, make_circle_right, rotate_angle_left, rotate_angle_right, set_left,
+             set_right, turn_straight_left, turn_straight_right]
--- web-server/static/scripts.js
+++ web-server/static/scripts.js
@@ -1,6 +1,58 @@
 /**
  * Created by Thomas on 05/10/2015.
  */
+var socket
+$(document).ready(function(){
+    //TODO: script on startup})
+    manueel = io.connect('http://' + document.domain + ':' + location.port + '/manueel');
+    beschrijving = io.connect('http://' + document.domain + ':' + location.port + '/beschrijving');
+    complex = io.connect('http://' + document.domain + ':' + location.port + '/complex');
+
+    manueel.on('alert', function(msg){window.alert("manueel meldt: " + JSON.stringify(msg));});
+    beschrijving.on('alert', function(msg){window.alert("beschrijving meldt: " + JSON.stringify(msg));});
+    complex.on('alert', function(msg){window.alert("complex meldt: " + JSON.stringify(msg));});
+
+
+    //here comes all the submit overrides
+
+    $('form#line').submit(function(event) {
+        complex.emit('line', {func:"line"});
+        return false;
+    });
+
+    $('form#square').submit(function(event) {
+        complex.emit('square', {func:"square"});
+        return false;
+    });
+
+    $('form#circle').submit(function(event) {
+        complex.emit('circle', {func:"circle"});
+        return false;
+    });
+
+    $('form#rStart').submit(function(event) {
+        beschrijving.emit('start',{});
+        return false;
+    });
+
+    $('form#rLeft').submit(function(event) {
+        beschrijving.emit('left', {nr: $("#NrLeft").val(), unit: $("#selectType").val()});
+        return false;
+    });
+
+    $('form#rRight').submit(function(event) {
+        beschrijving.emit('right', {nr: $("#NrRight").val(), unit: $("#selectType").val()});
+        return false;
+    });
+
+    $('form#rStop').submit(function(event) {
+        beschrijving.emit('stop', {nr: $("#NrStop").val(), unit: $("#selectType").val()});
+        return false;
+    });
+
+    //here comes responses on socket calls
+    //TODO: antwoorden opvangen
+});
 
 var keyDown = function(e){
     switch (e.keyCode) {
@@ -45,45 +97,92 @@ var leftKey = 37;
 var rightKey = 39;
 
 var doUp = function(){
-    document.getElementById("upB").id = "upA";
-    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify({function: "doUp"}), success:function(data){alert(JSON.parse(data));}});
+    manueel.emit("up",{status:'active'});
+    $("#up").toggleClass("active passive");
+    //var response = post({function: "doUp"}, getId);
 };
 
 var stopUp = function() {
-    document.getElementById("upA").id = "upB";
-    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify({function: "stopUp"}), success:function(data){alert(JSON.parse(data));}});
+    $("#up").toggleClass("active passive");
+    //var response = post({function: "stopUp"}, getId);
+    manueel.emit("up",{status:'passive'});
 };
 
 var doDown = function(){
-    document.getElementById("downB").id = "downA";
-    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify({function: "doDown"}), success:function(data){alert(JSON.parse(data));}});
+    $("#down").toggleClass("active passive");
+    //var response = post({function: "doDown"}, getId);
+    manueel.emit("down",{status:'active'});
 };
 
 var stopDown = function() {
-    document.getElementById("downA").id = "downB";
-    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify({function: "stopDown"}), success:function(data){alert(JSON.parse(data));}});
+    $("#down").toggleClass("active passive");
+    //var response = post({function: "stopDown"}, getId);
+    manueel.emit("down",{status:'passive'});
 };
 
 var doLeft = function(){
-    document.getElementById("leftB").id = "leftA";
-    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify({function: "doLeft"}), success:function(data){alert(JSON.parse(data));}});
+    $("#left").toggleClass("active passive");
+    //var response = post({function: "doLeft"}, getId);
+    manueel.emit("left",{status:'active'});
 };
 
 var stopLeft = function() {
-    document.getElementById("leftA").id = "leftB";
-    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify({function: "stopLeft"}), success:function(data){alert(JSON.parse(data));}});
+    $("#left").toggleClass("active passive");
+    //var response = post({function: "stopLeft"}, getId)
+    manueel.emit("left",{status:'passive'});
 };
 
 var doRight = function(){
-    document.getElementById("rightB").id = "rightA";
-    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify({function: "doRight"}), success:function(data){alert(JSON.parse(data));}});
+    $("#right").toggleClass("active passive");
+    //var result = post({function: "doRight"}, getId)
+    manueel.emit("right",{status:'active'});
 };
 
 var stopRight = function() {
-    document.getElementById("rightA").id = "rightB";
-    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify({function: "stopRight"}), success:function(data){alert(JSON.parse(data));}});
+    $("#right").toggleClass("active passive");
+    //var result = post({function: "stopRight"}, getId);
+    manueel.emit("right",{status:'passive'});
+    //window.alert(result)
+    //for(var key in result){window.alert(key); window.alert(result[key]);}
+    //alert(id)
 };
 
+var getId = function(response, status, xhr){//find a way to save the id that is returned (via global list or something)
+    try{
+        return response.id
+    }catch(err){
+        alert("an error has occured: " + err.message);
+        throw err;
+    }
+};
+
+var post = function(dataToSend, doOnSuccess){
+    doOnSuccess = doOnSuccess || function(){};
+    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify(dataToSend), success:doOnSuccess});
+    return response
+};
+
+var startStream = function(){
+    if($("#video_button").text() == "start stream") {
+        $("#video_button").text("stop stream");
+        var img = $("<img>").attr("src", Flask.url_for('video_feed'));
+        $("#video_stream").append(img)
+    }else{
+        $("#video_button").text("start stream");
+        /*$("#video_stream img").each(function(){
+            this.remove()
+        });*///TODO: werkt onerstaande? anders gebruik bovenstaande
+        $("#video_stream > img").each(function(){this.remove()})
+    }
+};
+
+var getAllfunctionsInQueue = function(){
+    //TODO: implement this function
+};
+
+var cancelFunction = function(id){
+    //TODO: implement this function
+};
 
 
 /*document.addEventListener('mousedown',function(){

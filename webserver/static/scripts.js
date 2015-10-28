b4276451a1756d6f6819fe4c004407154837c305
Thomas Michiels <mi_thom@hotmail.com>
2015-10-28 15:28:52 +0100

    we komen dichter bij

--- webserver/static/scripts.js
+++ webserver/static/scripts.js
@@ -118,106 +118,109 @@ $(document).ready(function(){
     document.addEventListener('keydown',keyDown,false);
     document.addEventListener('keyup', keyUp, false);
 
-    var upKey = 38;
-    var downKey = 40;
-    var leftKey = 37;
-    var rightKey = 39;
-
-    var doUp = function(){
-        manueel.emit("up",{status:'active'});
-        $("#up").toggleClass("active passive");
-        //var response = post({function: "doUp"}, getId);
-    };
-
-    var stopUp = function() {
-        $("#up").toggleClass("active passive");
-        //var response = post({function: "stopUp"}, getId);
-        manueel.emit("up",{status:'passive'});
-    };
-
-    var doDown = function(){
-        $("#down").toggleClass("active passive");
-        //var response = post({function: "doDown"}, getId);
-        manueel.emit("down",{status:'active'});
-    };
-
-    var stopDown = function() {
-        $("#down").toggleClass("active passive");
-        //var response = post({function: "stopDown"}, getId);
-        manueel.emit("down",{status:'passive'});
-    };
-
-    var doLeft = function(){
-        $("#left").toggleClass("active passive");
-        //var response = post({function: "doLeft"}, getId);
-        manueel.emit("left",{status:'active'});
-    };
-
-    var stopLeft = function() {
-        $("#left").toggleClass("active passive");
-        //var response = post({function: "stopLeft"}, getId)
-        manueel.emit("left",{status:'passive'});
-    };
-
-    var doRight = function(){
-        $("#right").toggleClass("active passive");
-        //var result = post({function: "doRight"}, getId)
-        manueel.emit("right",{status:'active'});
-    };
-
-    var stopRight = function() {
-        $("#right").toggleClass("active passive");
-        //var result = post({function: "stopRight"}, getId);
-        manueel.emit("right",{status:'passive'});
-        //window.alert(result)
-        //for(var key in result){window.alert(key); window.alert(result[key]);}
-        //alert(id)
-    };
-
-    var getId = function(response, status, xhr){//find a way to save the id that is returned (via global list or something)
-        try{
-            return response.id
-        }catch(err){
-            alert("an error has occured: " + err.message);
-            throw err;
-        }
-    };
-
-    var post = function(dataToSend, doOnSuccess){
-        doOnSuccess = doOnSuccess || function(){};
-        var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify(dataToSend), success:doOnSuccess});
-        return response
-    };
-
-    var startStream = function(){
-        if($("#video_button").text() == "start stream") {
-            $("#video_button").text("stop stream");
-            var img = $("<img>").attr("src", Flask.url_for('video_feed'));
-            $("#video_stream").append(img)
-        }else{
-            $("#video_button").text("start stream");
-            /*$("#video_stream img").each(function(){
-             this.remove()
-             });*///TODO: werkt onerstaande? anders gebruik bovenstaande
-            $("#video_stream > img").each(function(){this.remove()})
-        }
-    };
-
-    var getAllfunctionsInQueue = function(){
-        //TODO: implement this function
-    };
-
-    var cancelFunction = function(id){
-        //TODO: implement this function
-    };
-
-
-    /*document.addEventListener('mousedown',function(){
-     alert('hello world!');
-     });*/
-
-    /*$('#upKey').click(function(){
-     alert('hello world!')
-     });*/
-
 });
+
+
+
+//vanaf hier buiten document.ready()
+var upKey = 38;
+var downKey = 40;
+var leftKey = 37;
+var rightKey = 39;
+
+var doUp = function(){
+    manueel.emit("up",{status:'active'});
+    $("#up").toggleClass("active passive");
+    //var response = post({function: "doUp"}, getId);
+};
+
+var stopUp = function() {
+    $("#up").toggleClass("active passive");
+    //var response = post({function: "stopUp"}, getId);
+    manueel.emit("up",{status:'passive'});
+};
+
+var doDown = function(){
+    $("#down").toggleClass("active passive");
+    //var response = post({function: "doDown"}, getId);
+    manueel.emit("down",{status:'active'});
+};
+
+var stopDown = function() {
+    $("#down").toggleClass("active passive");
+    //var response = post({function: "stopDown"}, getId);
+    manueel.emit("down",{status:'passive'});
+};
+
+var doLeft = function(){
+    $("#left").toggleClass("active passive");
+    //var response = post({function: "doLeft"}, getId);
+    manueel.emit("left",{status:'active'});
+};
+
+var stopLeft = function() {
+    $("#left").toggleClass("active passive");
+    //var response = post({function: "stopLeft"}, getId)
+    manueel.emit("left",{status:'passive'});
+};
+
+var doRight = function(){
+    $("#right").toggleClass("active passive");
+    //var result = post({function: "doRight"}, getId)
+    manueel.emit("right",{status:'active'});
+};
+
+var stopRight = function() {
+    $("#right").toggleClass("active passive");
+    //var result = post({function: "stopRight"}, getId);
+    manueel.emit("right",{status:'passive'});
+    //window.alert(result)
+    //for(var key in result){window.alert(key); window.alert(result[key]);}
+    //alert(id)
+};
+
+var getId = function(response, status, xhr){//find a way to save the id that is returned (via global list or something)
+    try{
+        return response.id
+    }catch(err){
+        alert("an error has occured: " + err.message);
+        throw err;
+    }
+};
+
+var post = function(dataToSend, doOnSuccess){
+    doOnSuccess = doOnSuccess || function(){};
+    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify(dataToSend), success:doOnSuccess});
+    return response
+};
+
+var startStream = function(){
+    if($("#video_button").text() == "start stream") {
+        $("#video_button").text("stop stream");
+        var img = $("<img>").attr("src", Flask.url_for('video_feed'));
+        $("#video_stream").append(img)
+    }else{
+        $("#video_button").text("start stream");
+        /*$("#video_stream img").each(function(){
+         this.remove()
+         });*///TODO: werkt onerstaande? anders gebruik bovenstaande
+        $("#video_stream > img").each(function(){this.remove()})
+    }
+};
+
+var getAllfunctionsInQueue = function(){
+    //TODO: implement this function
+};
+
+var cancelFunction = function(id){
+    //TODO: implement this function
+};
+
+
+/*document.addEventListener('mousedown',function(){
+ alert('hello world!');
+ });*/
+
+/*$('#upKey').click(function(){
+ alert('hello world!')
+ });*/

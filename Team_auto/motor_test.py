48d0db7c6e5d6ed1ca7fa69c3d09de0e03e1105a
Gilles Moris <gilles.moris@student.kuleuven.be>
2015-11-03 17:37:36 +0100

    motor test update

--- Team_auto/motor_test.py
+++ Team_auto/motor_test.py
@@ -14,8 +14,8 @@ BrickPiSetupSensors()  # Send the properties of sensors to BrickPi
 
 def calibrate():
     print "start calibrate"
-    set_left(1)
-    set_right(1)
+    set_left(150)
+    set_right(150)
     offset_A= None
     offset_B= None
     while offset_A is None or offset_B is None:
@@ -26,20 +26,25 @@ def calibrate():
     return (offset_A, offset_B)
 
 def test(offset_A,offset_B):
-    left_power = power
-    right_power = power
-    set_left(left_power)
-    set_right(right_power)
-    BrickPiUpdateValues()
-    t = time.time()
-    while time.time() - t < 5:
-        pass
-    encoder_A = BrickPi.Encoder[PORT_A] - offset_A
-    encoder_B = BrickPi.Encoder[PORT_B] - offset_B
-    print 'Encoder A: ', encoder_A
-    print 'Encoder B: ', encoder_B
-    ratio = encoder_A / float(encoder_B)
-    print 'Ratio: ', ratio
+    list = [50,100,150,200,250]
+    for power in list:
+        left_power = power
+        right_power = power
+        set_left(left_power)
+        set_right(right_power)
+        BrickPiUpdateValues()
+        t = time.time()
+        while time.time() - t < 5:
+            pass
+        encoder_A = BrickPi.Encoder[PORT_A] - offset_A
+        encoder_B = BrickPi.Encoder[PORT_B] - offset_B
+        print 'Power: ', power
+        print '-------'
+        print 'Encoder A: ', encoder_A
+        print 'Encoder B: ', encoder_B
+        ratio = encoder_A / float(encoder_B)
+        print 'Ratio: ', ratio
+        time.sleep(5)
 
 time.sleep(2)
 (offset_A, offset_B) = calibrate()

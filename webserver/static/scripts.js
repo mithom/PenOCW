c7ee2a17df902a85783824c4a4a85c00d482ec02
Thomas Michiels <mi_thom@hotmail.com>
2015-11-05 14:17:08 +0100

    reformat javascript

--- webserver/static/scripts.js
+++ webserver/static/scripts.js
@@ -1,18 +1,18 @@
 /**
  * Created by Thomas on 05/10/2015.
-*/
+ */
 var manueel = null;
 var beschrijving = null;
 var complex = null;
 
 var last_update = 0;
 var timer = null;
-var updateTime = function(){
+var updateTime = function () {
     last_update += 0.1;
-    $("#last_updated_power").text(Math.round10(last_update,-1));
+    $("#last_updated_power").text(Math.round10(last_update, -1));
 };
 
-$(document).ready(function(){
+$(document).ready(function () {
     timer = setInterval(updateTime, 1000);
     manueel = io.connect('http://' + document.domain + ':' + location.port + '/manueel');
     beschrijving = io.connect('http://' + document.domain + ':' + location.port + '/beschrijving');
@@ -21,11 +21,13 @@ $(document).ready(function(){
     manueel.on('connect', function () {
         // window.alert('connected manueel');
         //manueel.on('alert', function(msg){window.alert("manueel meldt: " + JSON.stringify(msg));});
-        manueel.on('disconnect',function(){window.alert('manueel disconnected')});
+        manueel.on('disconnect', function () {
+            window.alert('manueel disconnected')
+        }); //TODO: via kleurencodering weergeven of socket verbonden is of niet en automatisch proberen te reconnecten
 
-        manueel.on('power', function(data){
+        manueel.on('power', function (data) {
             $("#value_power").text(data)
-            if(timer != null){
+            if (timer != null) {
                 clearInterval(timer);
                 last_update = 0;
                 $("#last_updated_power").text(0);
@@ -34,19 +36,19 @@ $(document).ready(function(){
         })
     });
 
-    beschrijving.on('connect',function(){
+    beschrijving.on('connect', function () {
         //beschrijving.on('alert', function(msg){window.alert("beschrijving meldt: " + JSON.stringify(msg));});
 
-        beschrijving.on('updateRouteDescription', function(route){
+        beschrijving.on('updateRouteDescription', function (route) {
             var routeBody = $("#currentRoute table tbody");
             routeBody.empty();
 
-            route.forEach(function(){
+            route.forEach(function () {
                 var newRow = document.createElement('tr');
                 var newName = document.createElement('td');
                 var newId = document.createElement('td');
                 var newIdContent = document.createTextNode('id: ' + route.id);
-                var newNameContent = document.createTextNode('command: '+ route.commandName);
+                var newNameContent = document.createTextNode('command: ' + route.commandName);
                 newName.appendChild(newNameContent);
                 newId.appendChild(newIdContent);
                 newRow.appendChild(newName);
@@ -56,43 +58,43 @@ $(document).ready(function(){
         });
     });
 
-    complex.on('connect',function(){
+    complex.on('connect', function () {
         //complex.on('alert', function(msg){window.alert("complex meldt: " + JSON.stringify(msg));});
     });
 
     //here comes all the submit overrides
 
-    $('form#line').submit(function(event) {
-        complex.emit('line', {func:"line"});
+    $('form#line').submit(function (event) {
+        complex.emit('line', {func: "line"});
         return false;
     });
 
-    $('form#square').submit(function(event) {
-        complex.emit('square', {func:"square"});
+    $('form#square').submit(function (event) {
+        complex.emit('square', {func: "square"});
         return false;
     });
 
-    $('form#circle').submit(function(event) {
-        complex.emit('circle', {func:"circle"});
+    $('form#circle').submit(function (event) {
+        complex.emit('circle', {func: "circle"});
         return false;
     });
 
-    $('form#rStart').submit(function(event) {
-        beschrijving.emit('start',{});
+    $('form#rStart').submit(function (event) {
+        beschrijving.emit('start', {});
         return false;
     });
 
-    $('form#rLeft').submit(function(event) {
+    $('form#rLeft').submit(function (event) {
         beschrijving.emit('left', {nr: $("#NrLeft").val(), unit: $("#selectType").val()});
         return false;
     });
 
-    $('form#rRight').submit(function(event) {
+    $('form#rRight').submit(function (event) {
         beschrijving.emit('right', {nr: $("#NrRight").val(), unit: $("#selectType").val()});
         return false;
     });
 
-    $('form#rStop').submit(function(event) {
+    $('form#rStop').submit(function (event) {
         beschrijving.emit('stop', {nr: $("#NrStop").val(), unit: $("#selectType").val()});
         return false;
     });
@@ -100,7 +102,7 @@ $(document).ready(function(){
     //here comes responses on socket calls
     //TODO: antwoorden opvangen
 
-    var keyDown = function(e){
+    var keyDown = function (e) {
         switch (e.keyCode) {
             case upKey:
                 doUp();
@@ -117,7 +119,7 @@ $(document).ready(function(){
         }
     };
 
-    var keyUp = function(e){
+    var keyUp = function (e) {
         switch (e.keyCode) {
             case upKey:
                 stopUp();
@@ -134,11 +136,10 @@ $(document).ready(function(){
         }
     };
 
-    document.addEventListener('keydown',keyDown,false);
+    document.addEventListener('keydown', keyDown, false);
     document.addEventListener('keyup', keyUp, false);
 
 });
-
 
 
 //vanaf hier buiten document.ready()
@@ -147,96 +148,106 @@ var downKey = 40;
 var leftKey = 37;
 var rightKey = 39;
 
-var doUp = function(){
-    manueel.emit("up",{status:'active'});
+var doUp = function () {
+    manueel.emit("up", {status: 'active'});
     $("#up").toggleClass("active passive");
     //var response = post({function: "doUp"}, getId);
 };
 
-var stopUp = function() {
+var stopUp = function () {
     $("#up").toggleClass("active passive");
     //var response = post({function: "stopUp"}, getId);
-    manueel.emit("up",{status:'passive'});
+    manueel.emit("up", {status: 'passive'});
 };
 
-var doDown = function(){
+var doDown = function () {
     $("#down").toggleClass("active passive");
     //var response = post({function: "doDown"}, getId);
-    manueel.emit("down",{status:'active'});
+    manueel.emit("down", {status: 'active'});
 };
 
-var stopDown = function() {
+var stopDown = function () {
     $("#down").toggleClass("active passive");
     //var response = post({function: "stopDown"}, getId);
-    manueel.emit("down",{status:'passive'});
+    manueel.emit("down", {status: 'passive'});
 };
 
-var doLeft = function(){
+var doLeft = function () {
     $("#left").toggleClass("active passive");
     //var response = post({function: "doLeft"}, getId);
-    manueel.emit("left",{status:'active'});
+    manueel.emit("left", {status: 'active'});
 };
 
-var stopLeft = function() {
+var stopLeft = function () {
     $("#left").toggleClass("active passive");
     //var response = post({function: "stopLeft"}, getId)
-    manueel.emit("left",{status:'passive'});
+    manueel.emit("left", {status: 'passive'});
 };
 
-var doRight = function(){
+var doRight = function () {
     $("#right").toggleClass("active passive");
     //var result = post({function: "doRight"}, getId)
-    manueel.emit("right",{status:'active'});
+    manueel.emit("right", {status: 'active'});
 };
 
-var stopRight = function() {
+var stopRight = function () {
     $("#right").toggleClass("active passive");
     //var result = post({function: "stopRight"}, getId);
-    manueel.emit("right",{status:'passive'});
+    manueel.emit("right", {status: 'passive'});
     //window.alert(result)
     //for(var key in result){window.alert(key); window.alert(result[key]);}
     //alert(id)
 };
 
-var getId = function(response, status, xhr){//find a way to save the id that is returned (via global list or something)
-    try{
+var getId = function (response, status, xhr) {//find a way to save the id that is returned (via global list or something)
+    try {
         return response.id
-    }catch(err){
+    } catch (err) {
         alert("an error has occured: " + err.message);
         throw err;
     }
 };
 
-var post = function(dataToSend, doOnSuccess){
-    doOnSuccess = doOnSuccess || function(){};
-    var response = $.ajax({url:'/', type: "POST", dataType:"json", contentType:'application/json; charset-utf-8', data:JSON.stringify(dataToSend), success:doOnSuccess});
+var post = function (dataToSend, doOnSuccess) {
+    doOnSuccess = doOnSuccess || function () {
+        };
+    var response = $.ajax({
+        url: '/',
+        type: "POST",
+        dataType: "json",
+        contentType: 'application/json; charset-utf-8',
+        data: JSON.stringify(dataToSend),
+        success: doOnSuccess
+    });
     return response
 };
 
-var startStream = function(){
-    if($("#video_button").text() == "start stream") {
+var startStream = function () {
+    if ($("#video_button").text() == "start stream") {
         $("#video_button").text("stop stream");
-        var img = $("<img>").attr("src", Flask.url_for('video_feed')).attr("height","120px");
+        var img = $("<img>").attr("src", Flask.url_for('video_feed')).attr("height", "120px");
         $("#video_stream").append(img)
-    }else{
+    } else {
         $("#video_button").text("start stream");
         /*$("#video_stream img").each(function(){
          this.remove()
          });*///TODO: werkt onerstaande? anders gebruik bovenstaande
-        $("#video_stream > img").each(function(){this.remove()})
+        $("#video_stream > img").each(function () {
+            this.remove()
+        })
     }
 };
 
-var getAllfunctionsInQueue = function(){
+var getAllfunctionsInQueue = function () {
     //TODO: implement this function
 };
 
-var cancelFunction = function(id){
+var cancelFunction = function (id) {
     //TODO: implement this function
 };
 
 // Closure to expand the Math module, source form: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Math/round
-(function() {
+(function () {//TODO: vervang heel deze code door: http://stackoverflow.com/questions/4098685/rounding-numbers-to-2-digits-after-comma
     /**
      * Decimal adjustment of a number.
      *
@@ -266,19 +277,19 @@ var cancelFunction = function(id){
 
     // Decimal round
     if (!Math.round10) {
-        Math.round10 = function(value, exp) {
+        Math.round10 = function (value, exp) {
             return decimalAdjust('round', value, exp);
         };
     }
     // Decimal floor
     if (!Math.floor10) {
-        Math.floor10 = function(value, exp) {
+        Math.floor10 = function (value, exp) {
             return decimalAdjust('floor', value, exp);
         };
     }
     // Decimal ceil
     if (!Math.ceil10) {
-        Math.ceil10 = function(value, exp) {
+        Math.ceil10 = function (value, exp) {
             return decimalAdjust('ceil', value, exp);
         };
     }

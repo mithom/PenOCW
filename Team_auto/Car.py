c1b7833a4be148e42f0f39ae651b59a3b6e608ca
Gilles Moris <gilles.moris@student.kuleuven.be>
2015-11-08 10:34:55 +0100

    PID fix test 1

--- Team_auto/Car.py
+++ Team_auto/Car.py
@@ -86,43 +86,41 @@ def go_straight_duration(power, duration):
 
     start_time = time.time()
     update_interval = 0.01
-    proportional_factor = 3
-    derivative_factor = 0.3
-    integral_factor = 4
+    # proportional_factor = 3
+    # derivative_factor = 0.3
+    # integral_factor = 4
+    proportional_factor = 1
+    derivative_factor = 1
+    integral_factor = 1
     pid_controller = PID.PID(proportional_factor, derivative_factor, integral_factor,
                              1, offset_A, offset_B, update_interval)
     last_update = time.time()
-    with open('values.txt', 'w') as f:
-        f.write('New PID --------')
-        while (time.time() - start_time) < duration:
-            if (time.time()-start_time) > step and main_power < power:
-                left_power += power_increase
-                right_power += power_increase
-                main_power += power_increase
-                step += 1
-            if time.time() - start_time > 0:
-                pid_controller.set_proportional(5*(main_power/power))
-            if time.time() - start_time > 1:
-                pid_controller.set_derivative(1)
-                pid_controller.set_integral(8)
-            encoder_A = BrickPi.Encoder[PORT_A] - offset_A
-            encoder_B = BrickPi.Encoder[PORT_B] - offset_B
-            if encoder_B != 0:
-                ratio = encoder_A / float(encoder_B)
-            else:
-                ratio = 1
-            f.write(str(ratio) + ',')
-            print 'Ratio: ', ratio
-            pid_ratio = pid_controller.update(BrickPi.Encoder[PORT_A], BrickPi.Encoder[PORT_B])
-            print 'PID ratio: ', pid_ratio
-            if (time.time() - last_update) > update_interval:
-                last_update = time.time()
-# right_power = int((2*main_power)/(pid_ratio+1))
-                right_power = int(main_power/(pid_ratio/2.0))
-                left_power = int((pid_ratio/2.0)*right_power)
-                set_motors(left_power, right_power) 
-            BrickPiUpdateValues()
-        print BrickPi.Encoder[PORT_A]
+    while (time.time() - start_time) < duration:
+        if (time.time()-start_time) > step and main_power < power:
+            left_power += power_increase
+            right_power += power_increase
+            main_power += power_increase
+            step += 1
+        # if time.time() - start_time > 0:
+        #     pid_controller.set_proportional(5*(main_power/power))
+        # if time.time() - start_time > 1:
+        #     pid_controller.set_derivative(1)
+        #     pid_controller.set_integral(8)
+        encoder_A = BrickPi.Encoder[PORT_A] - offset_A + 1000
+        encoder_B = BrickPi.Encoder[PORT_B] - offset_B + 1000
+        ratio = encoder_A / float(encoder_B)
+        print 'Ratio: ', ratio
+        pid_ratio = pid_controller.update(BrickPi.Encoder[PORT_A], BrickPi.Encoder[PORT_B])
+        print 'PID ratio: ', pid_ratio
+        if (time.time() - last_update) > update_interval:
+            last_update = time.time()
+            right_power = int((2*main_power)/(pid_ratio+1))
+            # right_power = int(main_power/(pid_ratio/2.0))
+            # left_power = int((pid_ratio/2.0)*right_power)
+            left_power = int(pid_ratio*right_power)
+        set_motors(left_power, right_power)
+        BrickPiUpdateValues()
+    print BrickPi.Encoder[PORT_A]
 
 
 def make_circle_left(power, radius):  # radius in cm

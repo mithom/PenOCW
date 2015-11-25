d35373a3e45b9e4fa2f5da3d5bb03e2a1214206f
Evy Rosseels <r0302418@namen.cs.kotnet.kuleuven.be>
2015-11-25 16:59:27 +0100

    pixels testversie + threshold

--- Beeldverwerking/pixels.py
+++ Beeldverwerking/pixels.py
@@ -4,14 +4,15 @@ import numpy as np
 import math
 import time
 from platform import system
+import urllib
 
 # Image loading
-#frame = cv.imread('/home/r0302418/Documents/PenO/street3.jpg', 1)
+frame = cv.imread('/home/r0302418/repos/penocw/Beeldverwerking/pi_photos/cam2.jpg', 1)
 
 # Stream capturing code copied from
 # http://stackoverflow.com/questions/24833149/track-objects-in-opencv-from-incoming-mjpeg-stream
 
-stream = urllib.urlopen('http://192.168.137.136:4848//video_feed.mjpg')
+'''stream = urllib.urlopen('http://192.168.137.136:4848//video_feed.mjpg')
 byte = ''
 while True:
     byte += stream.read(1024)
@@ -20,49 +21,57 @@ while True:
     if a != -1 and b != -1:
         jpg = byte[a:b+2]
         byte = byte[b+2:]
-        frame = cv.imdecode(np.fromstring(jpg, dtype=np.uint8), 0)
+        frame = cv.imdecode(np.fromstring(jpg, dtype=np.uint8), 0)'''
+# Image to grayscale
+gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
 
-	# Image to grayscale
-	gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+# Gaussian blur
+blur = cv.GaussianBlur(gray, (5, 5), 0)
 
-	# Gaussian blur
-	blur = cv.GaussianBlur(gray, (5, 5), 0)
+# Threshold voor bw bepalen adh gemiddelde grijswaarde over de foto
+hist = cv.calcHist([blur], [0], None, [32], [0, 256])
+for x in range(31, -1, -1):
+    if sum(hist[x] > 0):
+        threshold = (x-5)*8 + 4
+        break
 
-	# Threshold voor bw bepalen adh gemiddelde grijswaarde over de foto
-	average = np.average(blur)
+# Thresholding
+ret, bw = cv.threshold(blur, threshold, 255, cv.THRESH_BINARY)
 
-	# Thresholding
-	ret, bw = cv.threshold(blur, 200, 255, cv.THRESH_BINARY)
+# Cutting
+cut1 = bw[bw.shape[0]/3:bw.shape[0]]
+cut2 = cut1[:,cut1.shape[1]*1/4:cut1.shape[1]*3/4]
 
-	# Cutting
-	cut1 = bw[bw.shape[0]/2:bw.shape[0]]
-	cut2 = cut1[:,cut1.shape[1]*1/4:cut1.shape[1]*3/4]
+# Variable declaration
+img_width = cut2.shape[1]
+img_height = cut2.shape[0]
+img_division = 50
 
-	# Variable declaration
-	img_width = cut2.shape[1]
-	img_height = cut2.shape[0]
-	img_division = 15
+# Pixelation
+px = cv.resize(cut2, (img_width/img_division,img_height/img_division), interpolation = cv.INTER_NEAREST)
+print px.shape
+pxres = cv.resize(px, (img_width, img_height), interpolation = cv.INTER_NEAREST)
 
-	# Pixelation
-	px = cv.resize(cut2, (img_width/img_division,img_height/img_division), interpolation = cv.INTER_NEAREST)
-	print px.shape
-	pxres = cv.resize(px, (img_width, img_height), interpolation = cv.INTER_NEAREST)
+# Image showing
+show_original = 1
+show_bw = 2
+show_cut1 = 3
+show_cut2 = 4
+show_result = 5
+show_x = 5
 
-
-	# Image showing
-	show_original = False
-	show_bw = False
-	show_result = True
-
-	if show_original:
-	    cv.imshow('Result', frame)
-	    if cv.waitKey(1) == 27:
-            	exit(0)
-	elif show_bw:
-	    cv.imshow('Result', bw)
-	    if cv.waitKey(1) == 27:
-            	exit(0)
-	elif show_result:
-	    cv.imshow('Result', pxres)
-	    if cv.waitKey(1) == 27:
-            	exit(0)
+if show_x is show_original:
+    cv.imshow('Result', cv.resize(frame, (frame.shape[1]/4,frame.shape[0]/4),interpolation = cv.INTER_NEAREST))
+    cv.waitKey(0)
+elif show_x is show_bw:
+    cv.imshow('Result', cv.resize(bw, (bw.shape[1]/4,bw.shape[0]/4),interpolation = cv.INTER_NEAREST))
+    cv.waitKey(0)
+elif show_x is show_cut1:
+    cv.imshow('Result', cv.resize(cut1, (cut1.shape[1]/4,cut1.shape[0]/2),interpolation = cv.INTER_NEAREST))
+    cv.waitKey(0)
+elif show_x is show_cut2:
+    cv.imshow('Result', cv.resize(cut2, (cut2.shape[1]/2,cut2.shape[0]/2),interpolation = cv.INTER_NEAREST))
+    cv.waitKey(0)
+elif show_x is show_result:
+    cv.imshow('Result', cv.resize(pxres, (pxres.shape[1]/2,pxres.shape[0]/2),interpolation = cv.INTER_NEAREST))
+    cv.waitKey(0)

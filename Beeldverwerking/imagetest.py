cd93ba982c772448aefac7aeb34309a94f3b5adc
Gilles Moris <gilles.moris@student.kuleuven.be>
2015-11-25 17:30:29 +0100

    foto tests

--- Beeldverwerking/imagetest.py
+++ Beeldverwerking/imagetest.py
@@ -1,126 +1,48 @@
 import cv2 as cv
 import numpy as np
 import math
-import time
-from platform import system
 
-number_of_test_files = 6
-tape_width = 40
-lijn = [1,2,3]
-print lijn
-
-# Image loading
-# img = cv.imread('C:\\Users\\Gilles\\Desktop\\P&O\\line_images\\line6.jpg',1)
+number_of_test_files = 5
 
 imglist = []
-# for i in range(1,number_of_test_files + 1):
-for i in range(3, 4):
-    imglist.append(cv.imread('/home/r0450196/Desktop/nele_echte_lijn.jpg', 1))
+for i in range(0,number_of_test_files):
+    path_string = 'C:\\Users\\Gilles\\Dropbox\\Gilles\\Documenten\\School\\PenO_CW\\repo\\Beeldverwerking' +\
+                  '\\pi_photos\\cam' + str(i) + '.jpg'
+    print path_string
+    imglist.append(cv.imread(path_string, 1))
 
 for j in range(len(imglist)):
 
     img = imglist[j]
 
+    resized_image = cv.resize(img, (0, 0), None, 0.15, 0.15, cv.INTER_AREA)
+
     # Image to grayscale
-    gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+    gray = cv.cvtColor(resized_image, cv.COLOR_RGB2GRAY)
 
     # Gaussian blur
     blur = cv.GaussianBlur(gray, (5, 5), 0)
 
     # threshold voor bw bepalen adh gemiddelde grijswaarde over de foto
     average = np.average(blur)
-    print average
 
     # Thresholding
-    ret, bw = cv.threshold(blur, 200, 255, cv.THRESH_BINARY)
+    ret, bw = cv.threshold(blur, 150, 255, cv.THRESH_BINARY)
 
     # Canny edge detection
     edges = cv.Canny(bw, 5, 5)
 
     # Hough line transform
-    lines = cv.HoughLinesP(edges, 1, np.pi/180, tape_width, None, 1, 15)
-
-    # lines transformation
-    if system() == 'Linux':
-        new_lines = []
-        lines = lines[0]
-        for i in xrange(len(lines)):
-            new_lines.append([lines[i]])
-        lines = np.array(new_lines)
-
-    # Print number of found lines
-    print lines
-    print 'Lines found: ', len(lines)
+    lines = cv.HoughLinesP(edges, 2, np.pi/180, 61, None, 50, 200) #(edge image, rho, theta, threshold,
+                                                                    # lines, minLineLength, maxLineGap)
 
     # Filtering on/off
-    filtering = True
-    filtering2 = True
+    filtering = False
 
     # TODO: enkel beginpunt en rico opnieuw berekenen als index veranderd is
-################################################
-####### LIJN FILTERING 2
-#################################################
-    if filtering2 is True:
-        line1 = 0
-        while line1 < len(lines):
-            line2 = line1 + 1
-            while line2 < len(lines):
-                # Calculating the distance between the line starting points
-                line1_1 = [lines[line1][0][0], lines[line1][0][1]]
-                line1_2 = [lines[line1][0][2], lines[line1][0][3]]
-                line2_1 = [lines[line2][0][0], lines[line2][0][1]]
-                line2_2 = [lines[line2][0][2], lines[line2][0][3]]
-                mid_line_1 = [int((lines[line1][0][0] + lines[line1][0][2])/2), int((lines[line1][0][1] + lines[line1][0][3])/2)]
-                mid_line_2 = [int((lines[line2][0][0] + lines[line2][0][2])/2), int((lines[line2][0][1] + lines[line2][0][3])/2)]
-                distance = math.sqrt((mid_line_1[0]-mid_line_2[0])**2 + (mid_line_1[1]-mid_line_2[1])**2)
 
-                # Calculating line slopes
-                if max(line1_2[0], line1_1[0]) == line1_2[0]:
-                    dy = line1_2[1] - line1_1[1]
-                    dx = line1_2[0] - line1_1[0]
-                else:
-                    dy = line1_1[1] - line1_2[1]
-                    dx = line1_1[0] - line1_2[0]
-                if dx != 0:
-                    rico1 = dy / float(dx)
-                else:
-                    rico1 = 10000
-                if max(line2_2[0], line2_1[0]) == line2_2[0]:
-                    dy = line2_2[1] - line2_1[1]
-                    dx = line2_2[0] - line2_1[0]
-                else:
-                    dy = line2_1[1] - line2_2[1]
-                    dx = line2_1[0] - line2_2[0]
-                if dx != 0:
-                    rico2 = dy / float(dx)
-                else:
-                    rico2 = 10000
-
-                # Determining wether slopes approximately match
-                flag = 0
-                if (rico1 - rico2) < 7:
-                    flag = 1
-
-
-                # Filtering lines based on similar starting point and slope
-                delete = False
-                if distance < tape_width*2 and flag == 1:
-                    print lines[line1], lines[line2]
-                    new_line = [[int((lines[line1][0][0] + lines[line2][0][0])/2), int((lines[line1][0][1] + lines[line2][0][1])/2), int((lines[line1][0][2] + lines[line2][0][2])/2), int((lines[line1][0][3] + lines[line2][0][3])/2)]]
-                    lines[line1] = new_line
-                    lines = np.delete(lines, line2, axis = 0)
-                    delete = True
-                if not delete:
-                    line2 += 1
-            line1 += 1
-        print 'Lines after filtering2: ', len(lines)
-################################################
-####### LIJN FILTERING 2
-#################################################
-################################################
-####### LIJN FILTERING GILLES
-#################################################
-    if filtering is True:
+    # Line filtering
+    if filtering is True and lines is not None:
         line1 = 0
         while line1 < len(lines):
             line2 = line1 + 1
@@ -190,27 +112,19 @@ for j in range(len(imglist)):
                     line2 += 1
             line1 += 1
             line2 = 0
-        print 'Lines after filtering1: ', len(lines)
-
-################################################
-####### LIJN FILTERING GILLES
-#################################################
 
 
     # Grayscale to RGB for color line drawing
     line_image = cv.cvtColor(bw, cv.COLOR_GRAY2RGB)
 
-
-    # Grayscale to RGB for color line drawing
-    line_image = cv.cvtColor(bw, cv.COLOR_GRAY2RGB)
 
     # Drawing the lines
     if lines is not None:
         for k in xrange(0, len(lines)):
             for x1, y1, x2, y2 in lines[k]:
-                    # cv.line(line_image, (x1,y1),(x2,y2),(0,255,0),2)
                 cv.line(line_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     # Image showing
     cv.imshow('Lines', line_image)
+    cv.imshow('Original', resized_image)
     cv.waitKey(0)

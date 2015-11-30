3062cf9f2ae776a8e33133e0be38d26b683cdeda
Thomas Michiels <mi_thom@hotmail.com>
2015-11-30 15:35:44 +0100

    ça marche?

--- webserver/CarMock.py
+++ webserver/CarMock.py
@@ -38,12 +38,40 @@ def turn_straight_right(*args, **kwargs):
     time.sleep(0.1)
     pass
 
+def set_motors(left, right):
+    pass
+
+def go_straight_manual(*Args,**kwargs):
+    pass
+
+def go_straight_duration(*Args,**kwargs):
+    pass
+
+def rotate_left_angle(*Args,**kwargs):
+    pass
+
+def rotate_right_angle(*Args,**kwargs):
+    pass
+
+def rotate_left_duration(*Args,**kwargs):
+    pass
+
+def sleep(*Args,**kwargs):
+    pass
+
+def rotate_right_duration(*Args,**kwargs):
+    pass
+
+
+
 
 def get_functions():
-    functions = {'go_straight_distance': go_straight_distance, 'go_straight_pid': go_straight_pid,
-                    'make_circle_left': make_circle_left, 'make_circle_right': make_circle_right,
-                    'rotate_angle_left': rotate_angle_left, 'rotate_angle_right': rotate_angle_left,
-                    'turn_straight_left': turn_straight_left, 'turn_straight_right': turn_straight_right}
+    functions = {'go_straight_distance': go_straight_distance, 'go_straight_duration1': go_straight_duration,
+                 'go_straight_manual': go_straight_manual, 'make_circle_left': make_circle_left,
+                 'make_circle_right': make_circle_right, 'rotate_left_angle': rotate_left_angle,
+                 'rotate_right_angle': rotate_right_angle, 'rotate_left_duration': rotate_left_duration,
+                 'rotate_right_duration': rotate_right_duration, 'sleep': sleep,
+                 'set_powers': set_motors}
     return functions
 
 

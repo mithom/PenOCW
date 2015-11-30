21d6d025f17d19b15ddd9f664f9f3c80774d3edf
Gilles Moris <gilles.moris@student.kuleuven.be>
2015-11-30 15:34:26 +0100

    block update

--- Beeldverwerking/block.py
+++ Beeldverwerking/block.py
@@ -1,6 +1,11 @@
 class Block(object):
-    def __init__(self, coordinates=(0, 0)):
-        self.coordinate = coordinates
+    def __init__(self, left, right, top, bottom):
+        self.left = left
+        self.right = right
+        self.top = top
+        self.bottom = bottom
+
+        self.coordinate = ((right + left) / 2,(top + bottom) / 2)
 
     def getLocation(self):
         return self.coordinate

380070c67db9bfebf63b306a29fa10608e47ad63
Thomas Michiels <mi_thom@hotmail.com>
2015-11-30 17:54:28 +0100

    overzetten + code cleanup

--- Beeldverwerking/pixels.py
+++ Beeldverwerking/pixels.py
@@ -1,12 +1,15 @@
 # Importing needed modules
 import cv2 as cv
-import numpy as np
-import math
-import time
-from platform import system
-import urllib
 import block
 import copy
+from beeldverwerkingNameSpace import BeeldverwekingNameSpace
+from socketIO_client import SocketIO
+
+url = '192.168.137.136'
+port = 4848
+current_route_description = []
+socketIO = SocketIO(url, port)
+beeldverwerking_namespace = socketIO.define(BeeldverwekingNameSpace, '/beeldverwerking')
 
 # Image loading
 frame = cv.imread('/home/r0302418/repos/penocw/Beeldverwerking/pi_photos/cam4.jpg', 1)
@@ -34,15 +37,15 @@ blur = cv.GaussianBlur(gray, (5, 5), 0)
 hist = cv.calcHist([blur], [0], None, [32], [0, 256])
 for x in range(31, -1, -1):
     if sum(hist[x] > 0):
-        threshold = (x-8)*8 + 4
+        threshold = (x - 8) * 8 + 4
         break
 
 # Thresholding
 ret, bw = cv.threshold(blur, threshold, 255, cv.THRESH_BINARY)
 
 # Cutting
-cut1 = bw[bw.shape[0]/3:bw.shape[0]]
-cut2 = cut1[:,cut1.shape[1]*1/4:cut1.shape[1]*3/4]
+cut1 = bw[bw.shape[0] / 3:bw.shape[0]]
+cut2 = cut1[:, cut1.shape[1] * 1 / 4:cut1.shape[1] * 3 / 4]
 
 # Variable declaration
 img_width = cut2.shape[1]
@@ -50,11 +53,11 @@ img_height = cut2.shape[0]
 img_division = 50
 
 # Pixelation
-px = cv.resize(cut2, (img_width/img_division,img_height/img_division), interpolation = cv.INTER_NEAREST)
+px = cv.resize(cut2, (img_width / img_division, img_height / img_division), interpolation=cv.INTER_NEAREST)
 print px.shape
 
 # 
-#opgepast met breedte (blok ernaast)
+# opgepast met breedte (blok ernaast)
 min_width = 2
 max_width = 5
 min_length = 2
@@ -87,18 +90,20 @@ for r in xrange(px.shape[0]-1, 0, -1):
         stop_r = None
         
 print list_of_blocks
-  '''  
+  '''
+
 
 def remove_whites(px, left, right, bottom, top):
-    for i in xrange(left, right+1):
+    for i in xrange(left, right + 1):
         for j in xrange(bottom, top, -1):
-            print i,j
-            px[j][i]=0           
+            print i, j
+            px[j][i] = 0
     return px
 
+
 def check_right(index):
-    if index > (px.shape[1]-1):
-        index = (px.shape[1]-1)
+    if index > (px.shape[1] - 1):
+        index = (px.shape[1] - 1)
     return index
 
 
@@ -107,36 +112,36 @@ def check_top(index):
         index = 0
     return index
 
+
 pxbackup = copy.deepcopy(px)
-for r in xrange(px.shape[0]-1, 0, -1):
+for r in xrange(px.shape[0] - 1, 0, -1):
     found_white_row = False
-    for c in xrange(0,px.shape[1]-1,1):
+    for c in xrange(0, px.shape[1] - 1, 1):
         if px[r][c] == 255:
-            if r != 0 and c != (px.shape[1]-1):   
-                if px[r-1][c]==0 or px[r][c+1]==0:
-                    px[r][c]=0
+            if r != 0 and c != (px.shape[1] - 1):
+                if px[r - 1][c] == 0 or px[r][c + 1] == 0:
+                    px[r][c] = 0
                 else:
-                    wide_enough=False
-                    for i in xrange(min_width,max_width):
+                    wide_enough = False
+                    for i in xrange(min_width, max_width):
                         a = c + i
                         a = check_right(a)
-                        if px[r][a]==255:
-                            wide_enough=True
-                        if wide_enough==True:
+                        if px[r][a] == 255:
+                            wide_enough = True
+                        if wide_enough == True:
                             break
-                    long_enough=False
-                    for i in xrange(min_length,max_length):
+                    long_enough = False
+                    for i in xrange(min_length, max_length):
                         a = r - i
-                        a = check_top(a)                     
-                        if px[a][c]==255:
-                            long_enough=True
-                        if long_enough==True:
+                        a = check_top(a)
+                        if px[a][c] == 255:
+                            long_enough = True
+                        if long_enough == True:
                             break
-                    if long_enough==True and wide_enough==True:
-                        y = r - 1                        
-                        x = c + 1  
-                        while check_middle_x(y,x) == False or check_middle_y(y,x) == False:
-                            
+                    if long_enough == True and wide_enough == True:
+                        y = r - 1
+                        x = c + 1
+                        while check_middle_x(y, x) == False or check_middle_y(y, x) == False:
 
                         '''
                         a = c+4
@@ -152,9 +157,9 @@ print list_of_blocks[0].getLocation()
 
 for t in list_of_blocks:
     location = t.getLocation()
-    pxbackup[location[1]][location[0]]=150
+    pxbackup[location[1]][location[0]] = 150
 
-pxres = cv.resize(pxbackup, (img_width, img_height), interpolation = cv.INTER_NEAREST)
+pxres = cv.resize(pxbackup, (img_width, img_height), interpolation=cv.INTER_NEAREST)
 
 '''        
 white_rows = []
@@ -184,17 +189,22 @@ show_result = 5
 show_x = 5
 
 if show_x is show_original:
-    cv.imshow('Result', cv.resize(frame, (frame.shape[1]/4,frame.shape[0]/4),interpolation = cv.INTER_NEAREST))
+    cv.imshow('Result', cv.resize(frame, (frame.shape[1] / 4, frame.shape[0] / 4), interpolation=cv.INTER_NEAREST))
     cv.waitKey(0)
 elif show_x is show_bw:
-    cv.imshow('Result', cv.resize(bw, (bw.shape[1]/4,bw.shape[0]/4),interpolation = cv.INTER_NEAREST))
+    cv.imshow('Result', cv.resize(bw, (bw.shape[1] / 4, bw.shape[0] / 4), interpolation=cv.INTER_NEAREST))
     cv.waitKey(0)
 elif show_x is show_cut1:
-    cv.imshow('Result', cv.resize(cut1, (cut1.shape[1]/4,cut1.shape[0]/2),interpolation = cv.INTER_NEAREST))
+    cv.imshow('Result', cv.resize(cut1, (cut1.shape[1] / 4, cut1.shape[0] / 2), interpolation=cv.INTER_NEAREST))
     cv.waitKey(0)
 elif show_x is show_cut2:
-    cv.imshow('Result', cv.resize(cut2, (cut2.shape[1]/2,cut2.shape[0]/2),interpolation = cv.INTER_NEAREST))
+    cv.imshow('Result', cv.resize(cut2, (cut2.shape[1] / 2, cut2.shape[0] / 2), interpolation=cv.INTER_NEAREST))
     cv.waitKey(0)
 elif show_x is show_result:
-    cv.imshow('Result', cv.resize(pxres, (pxres.shape[1]/2,pxres.shape[0]/2),interpolation = cv.INTER_NEAREST))
+    cv.imshow('Result', cv.resize(pxres, (pxres.shape[1] / 2, pxres.shape[0] / 2), interpolation=cv.INTER_NEAREST))
     cv.waitKey(0)
+
+    ##########################
+    ## hier bebingt de stuur logica
+    ##########################
+    beeldverwerking_namespace.set_powers(0, 200)
